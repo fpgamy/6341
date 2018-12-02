9b4471142660aecea45421b0e79906b50d4c9fbd
fpgamy <mg3115@ic.ac.uk>
2018-12-01 20:59:52 -0500

    done

--- Project2/proj2.m
+++ Project2/proj2.m
@@ -17,26 +17,118 @@
 % Need R < L so that no samples are missing from the windows
 
 fid = fopen('gqrx_20181123_033424_939788000_2048000_fc.raw','rb');
-hw_freq = 940e6;
+hw_freq = 939788000;
 IQData = fread(fid,'float32');
 IQData = IQData(1:2:end) + 1i*IQData(2:2:end);
 fclose(fid);
 
 fs = 2.048e6;
-N  = 8192;
+% signal is bandlimited from 0 to 1.024e6
+% freqeuncy resolution ~= 1.024e6/L * 6db mainlobe width = 4k
+% for blackman window mainlobe is 12*pi/M
+% 4kHz/2.048MHz * 2*pi = 12*pi/(L-1)
+% L = 6 * 2.048Mhz / 4kHz = 3073
+% for sake of efficiency we shall choose N to be a power of 2 = 2048
+% for L = 4096, time resolution = 2048 / fs = 1ms
+N  = 4096;
 L  = N;
 R  = L/2;
-% pspectrum(abs(IQData), fs, 'spectrogram', 'FrequencyResolution', 4e3, ...
-%     'Overlap', 0, 'Leakage', 1);%, 'FrequencyLimits', [0.9e6 1e6]);%, FrequencyResolution', 125);
-Z = td_dft(transpose(IQData), R, L, N, blackman(L));
-spectrogram_db = 20.*log10(abs(Z)); 
-X = 1:N;
-Y = 1:(ceil(length(IQData)/R));
-figure;
-imagesc(X, Y, spectrogram_db);
+
+window = blackman(L);
+TDDFT = td_dft(transpose(IQData), R, L, N, window);
+periodogram_db = 20.*log10(abs(TDDFT)); 
+X = (hw_freq/1e6) + (((-fs/2):1:(fs/2))/(1e6));
+Y_labels = datenum(seconds((1:(ceil(length(IQData)/R)))*R/fs) + datetime(2018, 11, 23, 03, 34, 24));
+
+% Create figure
+figure1 = figure;
+
+% Create axes
+axes1 = axes('Parent',figure1);
+hold(axes1,'on');
+
+imagesc(X, Y_labels, periodogram_db, 'Parent',axes1);
+colorbar
+
+xlim(axes1,[(hw_freq - fs/2)/1e6  (hw_freq + fs/2)/1e6]);
+
 set(gca, 'YDir', 'normal');
+xlabel('Frequency (MHz)');
+datetick('y', 'mmm.dd,yyyy HH:MM:SS');
+ylim([Y_labels(1), Y_labels(end)])
 
 figure;
-plot(mean(spectrogram_db, 1));
-% surf(X, Y, abs(Z), 'edgecolor', 'none');
-% pspectrum(abs(IQData), fs);
+plot(20*log10(abs(fftshift(fft(window)))));
+
+figure;
+periodograms_single_time_inst = abs(TDDFT).*abs(TDDFT);
+unbias_sf = 1/(sum(abs(window).*abs(window)));
+periodograms_single_time_inst = periodograms_single_time_inst.*unbias_sf;
+unbiased_average_periodogram = mean(periodograms_single_time_inst);
+hold on;
+plot(10*log10(unbiased_average_periodogram));
+
+flat_periodogram = reshape(abs(TDDFT).*abs(TDDFT), 1, []);
+noise_cutoff_index = ceil(size(TDDFT, 1) * size(TDDFT, 2) / 5);
+td_dft_sorted = sort(flat_periodogram, 'ascend');
+noise_cutoff = unbias_sf * mean(td_dft_sorted(1:noise_cutoff_index));
+plot([0 N], [10*log10(noise_cutoff) 10*log10(noise_cutoff)]);
+hold off
+
+noise_cutoff_index = ceil(size(TDDFT, 1) * size(TDDFT, 2) / 1.1);
+td_dft_sorted = sort(flat_periodogram, 'ascend');
+noise_cutoff = unbias_sf * mean(td_dft_sorted(noise_cutoff_index));
+
+p = unbias_sf .* abs(TDDFT) .* abs(TDDFT);
+average_noise_periodogram = zeros(1, N);
+number_of_samples = zeros(1, N);
+for freq_bin = 1:1:N
+    freq_col = p(:,freq_bin);
+    number_of_samples(freq_bin) = sum(freq_col < noise_cutoff);
+    average_noise_periodogram(freq_bin) = mean(freq_col(freq_col < noise_cutoff));
+end
+
+figure;
+plot(10*log10(average_noise_periodogram));
+
+% Bayesian average
+bayesian_average_noise_periodogram = zeros(1, N);
+prior_periodogram = average_noise_periodogram;
+prior_periodogram(isnan(prior_periodogram)) = 0;
+
+PRIOR_W = N/32;
+for i = 1:10
+    for freq_bin = 1:1:N
+        if (freq_bin < ((PRIOR_W+1)/2) || freq_bin > (N-((PRIOR_W+1)/2)))
+            C = number_of_samples(freq_bin);
+            prior = prior_periodogram(freq_bin);
+        else
+            C = mean(number_of_samples(freq_bin-floor(PRIOR_W/2):freq_bin+(ceil((PRIOR_W+1)/2))));
+            prior = mean(prior_periodogram(freq_bin-floor(PRIOR_W/2):freq_bin+(ceil((PRIOR_W+1)/2))));
+        end
+
+        freq_col = p(:,freq_bin);
+        noise_samples = sum(freq_col < noise_cutoff);
+        bayesian_average_noise_periodogram(freq_bin) = (sum(freq_col(freq_col < noise_cutoff))+C*prior)/(noise_samples + C);
+    end
+    prior_periodogram = bayesian_average_noise_periodogram;
+end
+
+figure;
+hold on;
+plot(10*log10(bayesian_average_noise_periodogram));
+plot(10*log10(unbiased_average_periodogram));
+hold off;
+
+one_zero_periodogram = (abs(TDDFT).*abs(TDDFT));
+eta = 6/unbias_sf;
+for i = 1:N
+    fft_for_bin_i = movmean(one_zero_periodogram(:,i), 4);
+    one_locations = (find(fft_for_bin_i >  (eta*(bayesian_average_noise_periodogram(i))) ) );
+    ones_p = zeros(1, length(fft_for_bin_i));
+    ones_p(one_locations) = 1;
+    one_zero_periodogram(:,i) = ones_p;
+end
+
+figure;
+imagesc(one_zero_periodogram);
--- Project2/td_dft.m
+++ Project2/td_dft.m
@@ -5,27 +5,5 @@ function X = td_dft(x, R, L, N, w)
         x_r = transpose(x_padded(r:(L-1+r))).*w(1:L);
         X_rR = fftshift(fft(x_r, N));
         X(floor((r/R) + 1),:) = X_rR;
-        if ((r == 1) || (r >= -R+length(x)))
-            figure; plot(20*log10(abs(X_rR)))
-        end
     end
 end
-% x_padded = x;
-%     X = zeros(ceil((length(x)+L-1)/R), N);
-%     if (N < L)
-%         error("N must be greater than or equal to L");
-%     else
-%         if (N > L)
-%             x_padded = [x zeros(1, N-length(x))];
-%         end
-%     end
-%     for l = 1:N
-%         w_minus_n = w(length(w):-1:1);
-%         W = transpose(exp((1i*2*pi*l).*(1:length(w))./N));
-%         h_k = w_minus_n .* W;
-%         X_k = conv(x_padded, h_k);
-%         X_k = downsample(X_k, R);
-%         X(:,l) = X_k;
-%     end
-%     X = transpose(X);
-% end
